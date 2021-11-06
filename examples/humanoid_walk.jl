c439c7a78ae3d72aece7584b22f6e71145da08e1
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-11-06 10:31:33 -0400

    getting it to work on humanoid

--- examples/humanoid_walk.jl
+++ examples/humanoid_walk.jl
@@ -49,9 +49,10 @@ mvis = MechanismVisualizer(mechanism, URDFVisuals(urdf))
 settransform!(mvis.visualizer, Translation(0,0,0.9))
 # setelement!(mvis, footpoint, 0.01) 
 
-# desired_location = Point3D(root_frame(mechanism), 0.1, 0., 0.)
-# jacobian_transpose_ik!(state, body, footpoint, desired_location)
+# desired_location = Point3D(root_frame(mechanism), 0.5, 0., -0.9)
+# jacobian_transpose_ik!(state, rbody, rfootpoint, desired_location)
 # set_configuration!(mvis, configuration(state))
+# println("leg at ", transform(state, rfootpoint, root_frame(mechanism)))
 
 init_position = [0.0, 0.0, 0.0]
 goal_position = [5.0, 0.0, 0.0]
@@ -62,18 +63,44 @@ xl,yl,zl = x[3], x[4], x[5]
 xr,yr,zr = x[6], x[7], x[8]
 
 anim = Animation()
-for i=1:length(xb)
+x = [0.3]
+for i=1:3#length(xb)
     atframe(anim, i) do 
-        # settransform!(mvis.visualizer, Translation(xb[i], yb[i], 0.9))
-        ld = Point3D(root_frame(mechanism), xl[i], yl[i], zl[i])
+        settransform!(mvis.visualizer, Translation(x[1], 0, 0.9))
+        ld = Point3D(root_frame(mechanism), x[1], -0.15, -0.9) 
         jacobian_transpose_ik!(state, lbody, lfootpoint, ld)
         set_configuration!(mvis, configuration(state) )
-        rd = Point3D(root_frame(mechanism), xr[i], yr[i], zr[i])
+        rd = Point3D(root_frame(mechanism), x[1], 0.15, -0.9) 
         jacobian_transpose_ik!(state, rbody, rfootpoint, rd)
         set_configuration!(mvis, configuration(state) )
+        x[1] = x[1].+0.1
     end
 end
 setanimation!(mvis, anim)
+
+
+# init_position = [0.0, 0.0, 0.0]
+# goal_position = [5.0, 0.0, 0.0]
+# x = move_to_position(init_position; goal_position=goal_position)
+
+# xb,yb,zb = x[1], x[2], ones(length(x[1]))
+# xl,yl,zl = x[3], x[4], x[5]
+# xr,yr,zr = x[6], x[7], x[8]
+
+# anim = Animation()
+# for i=1:length(xb)
+#     atframe(anim, i) do 
+#         settransform!(mvis.visualizer, Translation(xb[i], yb[i], 0.9))
+#         ld = Point3D(root_frame(mechanism), xl[i], yl[i], -0.9)
+#         println(ld)
+#         jacobian_transpose_ik!(state, lbody, lfootpoint, ld)
+#         set_configuration!(mvis, configuration(state) )
+#         rd = Point3D(root_frame(mechanism), xr[i], yr[i], -0.9)
+#         jacobian_transpose_ik!(state, rbody, rfootpoint, rd)
+#         set_configuration!(mvis, configuration(state) )
+#     end
+# end
+# setanimation!(mvis, anim)
 # qs = typeof(configuration(state))[]
 # for x in range(-0.2, stop=0.2, length=100)
 #     desired = Point3D(root_frame(mechanism), x, 0., -0.9)
